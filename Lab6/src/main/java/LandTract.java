0dcc8977e8b8d515c659c6ca90fc3546483659c2
theki <theki@DESKTOP-530HBLH>
2020-04-29 20:22:18 -0500

    part 1 is done

--- Lab6/src/main/java/LandTract.java
+++ Lab6/src/main/java/LandTract.java
@@ -1,4 +1,54 @@
 
 public class LandTract {
+    double length;
+    double width;
+    double area;
+    String name;
+
+    public LandTract(String name,double length, double width) {
+        this.length = length;
+        this.width = width;
+        this.name =name;
+    }
+    
+    public double getArea(){
+       this.area=this.length *this.width;
+        return area;
+    }
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "LandTract{"+"name= "+name +",length=" + length + ", width=" + width+ " area=" + area + '}';
+    }
+
+    
+   
+
+    
+    
+    
     
 }
--- Lab6/src/main/java/Main.java
+++ Lab6/src/main/java/Main.java
@@ -1,7 +1,33 @@
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter land 1 name");
+        String land1name = keyboard.next();
+        System.out.println("Please enter land 1 length");
+        double land1length = keyboard.nextDouble();
+        System.out.println("Please enter land 1 width");
+        double land1width = keyboard.nextDouble();
+        System.out.println("Please enter land 2 name");
+        String land2name = keyboard.next();
+        System.out.println("Please enter land 2 length");
+        double land2length = keyboard.nextDouble();
+        System.out.println("Please enter land 2 width");
+        double land2width = keyboard.nextDouble();
+        LandTract land1 = new LandTract(land1name, land1length, land1width);
+        LandTract land2 = new LandTract(land2name, land2length, land2width);
+        System.out.println(land1name + "Area is " + land1.getArea());
+        System.out.println(land1name + "Area is " + land2.getArea());
+        if (land1.getArea() == land2.getArea()) {
+            System.out.println(land1name + " and " + land2name + " is equal");
+        } else {
+            System.out.println(land1name + " and " + land2name + " is not equal");
+        }
+        System.out.println(land1.toString());
+        System.out.println(land2);
+
     }
-   
 }
